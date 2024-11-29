a22c61e33c8c79ce4c57257f154ea0410ed1d09d
caoimhe <caoimh3cahill@gmail.com>
2024-11-29 00:02:22 +0000

    Initial Design of login. Functionality needed

--- angular-frontend/src/app/login/login.component.ts
+++ angular-frontend/src/app/login/login.component.ts
@@ -1,28 +1,42 @@
-import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {Router} from '@angular/router';
-import {NgIf} from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Title} from '@angular/platform-browser';
+import {NgIf, NgOptimizedImage} from '@angular/common';
+import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
+import {MatInput} from '@angular/material/input';
+import {MatToolbar} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     FormsModule,
-    NgIf
+    NgIf,
+    MatFormField,
+    MatButton,
+    RouterLink,
+    MatInput,
+    MatError,
+    MatLabel,
+    MatToolbar,
+    ReactiveFormsModule,
+    NgOptimizedImage
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
-  email: string = '';
-  password: string = '';
+export class LoginComponent implements OnInit{
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  loginData = { email: '', password: '' };
+  constructor(private titleService: Title) {}
 
-  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.titleService.setTitle('Login');
+  }
 
   onSubmit() {
-    if (this.email && this.password) {
-      console.log('Logging in with:', this.email, this.password);
-      // Add authentication logic here
-    }
+
   }
 }
