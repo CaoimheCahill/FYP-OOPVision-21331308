d6cfc854363b7810d4aea107047ee267767d62a9
caoimhe <caoimh3cahill@gmail.com>
2024-12-02 17:44:43 +0000

    Adding basic functionality

--- angular-frontend/src/app/login/login.component.ts
+++ angular-frontend/src/app/login/login.component.ts
@@ -4,7 +4,7 @@ import {Title} from '@angular/platform-browser';
 import {NgIf, NgOptimizedImage} from '@angular/common';
 import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatButton} from '@angular/material/button';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {MatInput} from '@angular/material/input';
 import {MatToolbar} from '@angular/material/toolbar';
 
@@ -30,13 +30,23 @@ import {MatToolbar} from '@angular/material/toolbar';
 export class LoginComponent implements OnInit{
   readonly email = new FormControl('', [Validators.required, Validators.email]);
   loginData = { email: '', password: '' };
-  constructor(private titleService: Title) {}
+  constructor(private titleService: Title, private router: Router) {}
 
   ngOnInit(): void {
     this.titleService.setTitle('Login');
   }
 
-  onSubmit() {
+  onSubmit(): void {
+    if (this.loginData.email && this.loginData.password) {
+      console.log('Login Data:', this.loginData);
 
+      // Simulate a successful login
+      alert('Login successful!');
+
+      // Navigate to the HomeComponent after successful login
+      this.router.navigate(['/home']);
+    } else {
+      alert('Please fill in all required fields');
+    }
   }
 }
