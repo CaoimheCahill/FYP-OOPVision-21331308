0204e2514d8f237bd817d8cdfaecacb876b1dd80
caoimhe <caoimh3cahill@gmail.com>
2024-12-04 10:28:33 +0000

    User Repository

--- spring-boot-backend/src/main/java/com/example/springbootbackend/model/User.java
+++ spring-boot-backend/src/main/java/com/example/springbootbackend/model/User.java
@@ -11,7 +11,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
     private String firstName;
     private String lastName;
@@ -19,11 +19,11 @@ public class User {
     private String password;
     private String userRole;
 
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
---
+++ spring-boot-backend/src/main/java/com/example/springbootbackend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.springbootbackend.repository;
+
+import com.example.springbootbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(int id);
+}
