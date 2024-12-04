9f593e071936458d5b0b5ab05f022aa2900d596b
caoimhe <caoimh3cahill@gmail.com>
2024-12-04 13:49:56 +0000

    quickfix: test issue

--- spring-boot-backend/src/test/java/com/example/springbootbackend/SpringBootBackendApplicationTests.java
+++ spring-boot-backend/src/test/java/com/example/springbootbackend/SpringBootBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.springbootbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBootBackendApplicationTests {
 
     @Test
