1eec8375ffdbce35e0870a5b461dc22fb368f245
caoimhe <caoimh3cahill@gmail.com>
2024-12-04 10:33:05 +0000

    Basic User Controller and Service

---
+++ spring-boot-backend/src/main/java/com/example/springbootbackend/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.springbootbackend.service;
+
+import com.example.springbootbackend.model.User;
+import com.example.springbootbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists!");
+        }
+        return userRepository.save(user);
+    }
+}
+
