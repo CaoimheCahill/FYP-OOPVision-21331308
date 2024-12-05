dc5b28a131af0686d46d792488cbc227dc76d7ef
caoimhe <caoimh3cahill@gmail.com>
2024-12-05 13:14:37 +0000

    Reverting changes

--- angular-frontend/src/app/app.config.ts
+++ angular-frontend/src/app/app.config.ts
@@ -1,16 +1,11 @@
 import { provideRouter } from '@angular/router';
-import {APP_INITIALIZER, ApplicationConfig} from '@angular/core';
+import {ApplicationConfig} from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { routes } from './app.routes';
-import {ConfigService} from './service/config.service';
-
-
+import {routes} from "./app.routes"
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
+  providers: [provideRouter(routes),
     provideHttpClient(),
-    provideAnimations(),
-  ],
+    provideAnimations(),]
 };
--- angular-frontend/src/app/service/user.service.ts
+++ angular-frontend/src/app/service/user.service.ts
@@ -1,23 +1,22 @@
-import {Inject, Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../environments/environment.prod';
-import {ConfigService} from "./config.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  //private baseUrl = environment.apiBaseUrl + '/api/users';
+  private baseUrl = environment.apiBaseUrl + '/api/users';
 
-  constructor(private http: HttpClient, private configService: ConfigService) { }
+  constructor(private http: HttpClient) { }
 
   register(user: any): Observable<any>{
-    return this.http.post(`${this.configService.getApiBaseUrl()}/api/users/register`, user)
+    return this.http.post(`${this.baseUrl}/register`, user)
   }
 
   login(loginData: { email: string; password: string }): Observable<any> {
-    return this.http.post(`${this.configService.getApiBaseUrl()}/api/users/login`, loginData, {
+    return this.http.post(`${this.baseUrl}/login`, loginData, {
       responseType: 'arraybuffer',
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     });
