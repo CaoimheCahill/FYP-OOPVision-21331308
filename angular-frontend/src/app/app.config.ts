93ef185278b8a8251f8268bbd52268b7502b9441
caoimhe <caoimh3cahill@gmail.com>
2024-12-05 12:47:03 +0000

    quickfix: trying another alternative solution to azure env variables

--- angular-frontend/src/app/app.config.ts
+++ angular-frontend/src/app/app.config.ts
@@ -1,24 +1,20 @@
 import { provideRouter } from '@angular/router';
-import { ApplicationConfig } from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig} from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import {ConfigService} from './service/config.service';
 
-// Function to retrieve the `apiBaseUrl` dynamically
-function getApiBaseUrl(): string {
-  const apiBaseUrl = (window as any).apiBaseUrl;
-  if (!apiBaseUrl) {
-    console.warn('API_BASE_URL is not set. Using fallback.');
-  }
-  return apiBaseUrl || 'http://localhost:8080/api'; // Fallback
+function initializeApp(configService: ConfigService): () => Promise<void> {
+  return () => configService.loadConfig();
 }
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: APP_INITIALIZER, useFactory: initializeApp, deps: [ConfigService]},
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
-    { provide: 'API_BASE_URL', useFactory: getApiBaseUrl }, // Provide API_BASE_URL dynamically
   ],
 };
--- angular-frontend/src/app/service/user.service.ts
+++ angular-frontend/src/app/service/user.service.ts
@@ -2,6 +2,7 @@ import {Inject, Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../environments/environment.prod';
+import {ConfigService} from "./config.service";
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,14 @@ import {environment} from '../environments/environment.prod';
 export class UserService {
   //private baseUrl = environment.apiBaseUrl + '/api/users';
 
-  constructor(private http: HttpClient, @Inject('API_BASE_URL') private baseUrl: string) { }
+  constructor(private http: HttpClient, private configService: ConfigService) { }
 
   register(user: any): Observable<any>{
-    return this.http.post(`${this.baseUrl}/api/users/register`, user)
+    return this.http.post(`${this.configService.apiBaseUrl}/api/users/register`, user)
   }
 
   login(loginData: { email: string; password: string }): Observable<any> {
-    return this.http.post(`${this.baseUrl}/api/users/login`, loginData, {
+    return this.http.post(`${this.configService.apiBaseUrl}/api/users/login`, loginData, {
       responseType: 'arraybuffer',
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     });
