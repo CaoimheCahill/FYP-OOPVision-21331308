d8e1ddfa514d4df6d3c7751393edf986bdfee390
caoimhe <caoimh3cahill@gmail.com>
2024-12-10 19:54:30 +0000

    Frontend topic service basic implementation

--- angular-frontend/src/app/service/topic.service.ts
+++ angular-frontend/src/app/service/topic.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../environments/environment.prod';
+
+export interface Topic {
+  id: number;
+  name: string;
+  description: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TopicService {
 
-  constructor() { }
+  private apiUrl = environment.apiBaseUrl + '/api/topics'; // Adjust URL if necessary
+
+  constructor(private http: HttpClient) {}
+
+  getTopics(): Observable<Topic[]> {
+    return this.http.get<Topic[]>(this.apiUrl);
+  }
 }
