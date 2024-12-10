898c72c5d719a68327a1493697c27ff15533872c
caoimhe <caoimh3cahill@gmail.com>
2024-12-10 17:13:49 +0000

    Start of home page design

--- angular-frontend/src/app/home/home.component.ts
+++ angular-frontend/src/app/home/home.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+    imports: [
+        MatButtonModule,
+        MatToolbarModule,
+        NgOptimizedImage,
+        RouterLink
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
