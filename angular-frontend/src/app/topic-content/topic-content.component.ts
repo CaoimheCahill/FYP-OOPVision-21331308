f6f27c402930dcd2282d1914353ff5b8ebcbccea
caoimhe <caoimh3cahill@gmail.com>
2024-12-10 20:47:30 +0000

    topic content component generated

---
+++ angular-frontend/src/app/topic-content/topic-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './topic-content.component.html',
+  styleUrl: './topic-content.component.scss'
+})
+export class TopicContentComponent {
+
+}
