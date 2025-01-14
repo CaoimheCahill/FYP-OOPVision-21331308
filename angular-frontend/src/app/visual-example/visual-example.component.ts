659b4ba2ef86935f8ffc428d61adc4f3cfadad23
caoimhe <caoimh3cahill@gmail.com>
2025-01-14 18:33:36 +0000

    Navbar, footer and back button

--- angular-frontend/src/app/visual-example/visual-example.component.ts
+++ angular-frontend/src/app/visual-example/visual-example.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {NgIf, NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-visual-example',
   standalone: true,
-  imports: [],
+    imports: [
+        MatButtonModule,
+        MatToolbarModule,
+        NgOptimizedImage,
+        RouterLink
+    ],
   templateUrl: './visual-example.component.html',
   styleUrl: './visual-example.component.scss'
 })
-export class VisualExampleComponent {
+export class VisualExampleComponent implements OnInit{
+
+  constructor(private titleService: Title) {
+  }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Visual Example');
+  }
 
 }
