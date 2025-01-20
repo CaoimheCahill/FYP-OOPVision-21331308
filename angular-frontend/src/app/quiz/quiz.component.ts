42291385fb58dff545a549ab90dcd0a556763c18
caoimhe <caoimh3cahill@gmail.com>
2025-01-20 17:36:53 +0000

    Navbar + footer

--- angular-frontend/src/app/quiz/quiz.component.ts
+++ angular-frontend/src/app/quiz/quiz.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-quiz',
   standalone: true,
-  imports: [],
+    imports: [
+        MatButtonModule,
+        MatToolbarModule,
+        NgOptimizedImage,
+        RouterLink
+    ],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.scss'
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit{
+
+  constructor(private titleService: Title) {
+  }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Quiz');
+
+  }
 
 }
