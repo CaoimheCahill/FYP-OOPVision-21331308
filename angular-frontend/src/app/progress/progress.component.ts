a5c53b85761f7ae63712637ee5b91eeba69d01d2
caoimhe <caoimh3cahill@gmail.com>
2025-02-12 11:43:55 +0000

    User Progress header, footer and name

--- angular-frontend/src/app/progress/progress.component.ts
+++ angular-frontend/src/app/progress/progress.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-progress',
   standalone: true,
-  imports: [],
+    imports: [
+        MatButtonModule,
+        MatToolbarModule,
+        NgOptimizedImage,
+        RouterLink
+    ],
   templateUrl: './progress.component.html',
   styleUrl: './progress.component.scss'
 })
-export class ProgressComponent {
+export class ProgressComponent implements OnInit{
+
+  constructor(private titleService: Title) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Progress');
+  }
 
 }
