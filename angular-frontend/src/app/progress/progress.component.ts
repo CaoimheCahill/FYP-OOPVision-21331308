62c6f93b29ddf962b579bc3e9bc0e55e45f03f4c
caoimhe <caoimh3cahill@gmail.com>
2025-03-15 22:24:02 +0000

    Progress frontend - fixes needed

--- angular-frontend/src/app/progress/progress.component.ts
+++ angular-frontend/src/app/progress/progress.component.ts
@@ -1,28 +1,59 @@
 import {Component, OnInit} from '@angular/core';
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
-import {NgOptimizedImage} from "@angular/common";
+import {CommonModule, DecimalPipe, NgOptimizedImage} from "@angular/common";
 import {RouterLink} from "@angular/router";
 import {Title} from '@angular/platform-browser';
+import {ProgressService} from '../service/progress.service';
+import {TopicService} from '../service/topic.service';
 
 @Component({
   selector: 'app-progress',
   standalone: true,
-    imports: [
-        MatButtonModule,
-        MatToolbarModule,
-        NgOptimizedImage,
-        RouterLink
-    ],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    NgOptimizedImage,
+    RouterLink,
+    DecimalPipe
+  ],
   templateUrl: './progress.component.html',
   styleUrl: './progress.component.scss'
 })
 export class ProgressComponent implements OnInit{
 
-  constructor(private titleService: Title) {}
+  progressData: any = {};   // Store the progress data
+  completionPercentage: number = 0;
+  totalTopics: number = 0;
+
+  constructor(private titleService: Title, private progressService: ProgressService, private topicService: TopicService) {}
 
   ngOnInit(): void {
+    const userId = 1;
     this.titleService.setTitle('Progress');
+
+    // Fetch the progress data
+    this.progressService.getUserProgress(userId).subscribe((data) => {
+      this.progressData = data.progress;
+      this.completionPercentage = data.completionPercentage;
+      console.log(this.progressData)
+    });
+
+    // Fetch the total number of topics
+    this.topicService.getTotalTopics().subscribe((totalTopics) => {
+      this.totalTopics = totalTopics;  // Set the total topics value
+    });
   }
+
+  get circumference() {
+    const radius = 80;
+    return 2 * Math.PI * radius;
+  }
+
+  get offset() {
+    return this.circumference * (1 - this.completionPercentage / 100);
+  }
+
 
 }
