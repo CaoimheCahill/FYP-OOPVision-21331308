9e17cb1b16e0b26e4f4b383e015cb36d33b41b4e
caoimhe <caoimh3cahill@gmail.com>
2025-04-08 03:29:28 +0100

    getting rid of unnecessary page

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -4,7 +4,6 @@ import {RegistrationComponent} from './registration/registration.component';
 import {HomeComponent} from './home/home.component';
 import {TopicsComponent} from './topics/topics.component';
 import {ProgressComponent} from './progress/progress.component';
-import {EditProfileComponent} from './edit-profile/edit-profile.component';
 import {TopicContentComponent} from './topic-content/topic-content.component';
 import {VisualExampleComponent} from './visual-example/visual-example.component';
 import {QuizComponent} from './quiz/quiz.component';
@@ -25,7 +24,6 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'topics', component: TopicsComponent },
   { path: 'progress', component: ProgressComponent },
-  { path: 'editProfile', component: EditProfileComponent },
   { path: 'topic/:id', component: TopicContentComponent },
   { path: 'topic/:topicId/visualExample/:visualExampleId', component: VisualExampleComponent },
   { path: 'topic/:topicId/quiz', component: QuizComponent },
