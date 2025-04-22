bc40707a0982db54bc6422021ec250d2f99465ff
caoimhe <caoimh3cahill@gmail.com>
2025-04-22 12:03:11 +0100

    hot fix: revert changes

--- spring-boot-backend/src/main/java/com/example/springbootbackend/config/JwtUtil.java
+++ spring-boot-backend/src/main/java/com/example/springbootbackend/config/JwtUtil.java
@@ -3,21 +3,17 @@ package com.example.springbootbackend.config;
 import com.example.springbootbackend.model.User;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    @Value("${app.jwt.secret}")
-    private String secret;
-
     private Key getSigningKey() {
-        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        String SECRET = "ik8cFZzqEq4ZqFj6uDs4j39VtRl0Cw8+lfvGZScqpSM=";
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
     }
 
     public String generateToken(User user) {
